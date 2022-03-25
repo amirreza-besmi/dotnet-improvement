1ef82cbfe1bb47c9c7fe05b7fd2cf5ba8ea63336
amirreza besmi <amirreza.besmi.work@gmail.com>
2022-03-25 15:02:29 +0430

    perf(user): add User model

---
+++ dotnet-improvement/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_improvement.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        public decimal CreditAmount { get; set; }
+    }
+}
