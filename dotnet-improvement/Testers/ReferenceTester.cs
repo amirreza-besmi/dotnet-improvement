0441ea87a296a1886ea649043254b002b151351c
amirreza besmi <amirreza.besmi.work@gmail.com>
2022-09-25 09:25:31 +0330

    feat(Reference): apply some updates about string reference tester

--- dotnet-improvement/Testers/ReferenceTester.cs
+++ dotnet-improvement/Testers/ReferenceTester.cs
@@ -9,10 +9,13 @@ namespace dotnet_improvement.Presentation.Testers
         public static void TestStringReferences()
         {
             string str1 = "Hi";
+            string str2 = "Hi";
+
             // not assigned by reference because create new instance with the same value:
-            string str2 = String.Copy(str1);
+            string str3 = String.Copy(str1);
 
             Console.WriteLine(Object.ReferenceEquals(str1, str2));
+            Console.WriteLine(Object.ReferenceEquals(str1, str3));
         }
     }
 }
